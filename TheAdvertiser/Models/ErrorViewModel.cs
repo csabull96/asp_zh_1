b7010b3468af3ff6798c533257ca0f5e8939a92d
Csaba Sandor <csabull96@gmail.com>
2020-10-23 21:04:58 +0200

    01 - project created

---
+++ TheAdvertiser/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TheAdvertiser.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
